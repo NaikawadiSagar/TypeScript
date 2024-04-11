180abb62ef3a5f65e946e0380d6a3af440a00d72
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-04-11 09:19:32 +0530

    First TypeScript Program

---
+++ Basic/01 First.ts
@@ -0,0 +1,20 @@
+// var A1= 20;
+// var A2: number = 21;
+
+// console.log(A1);
+// console.log(A2);
+
+// var Strng = "Jay Ganesh";
+// console.log(Strng);
+// console.log(typeof Strng);
+
+// var No = 9;
+// console.log(No);
+// console.log(typeof No);
+function Fun(No)
+{
+    console.log(No);
+}
+
+var No1:number =20;
+Fun(No1);
