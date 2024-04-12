2f6ffdb8bee447856c0ca24ed87d851cc32f7338
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-04-12 21:58:10 +0530

    Second TypeScript Program

---
+++ Basic/02 Function.ts
@@ -0,0 +1,15 @@
+var No1: number = 20;
+var No2: number = 15;
+var Ret:number = 0;
+
+
+Ret = Sum(No1,No2);
+
+console.log("Additon is " + Ret);
+
+function Sum(No1:number,No2:number):number {
+    var Ans = 0;
+    Ans = No1 + No2
+    return Ans;
+}
+    
