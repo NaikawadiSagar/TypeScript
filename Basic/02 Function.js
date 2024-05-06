d205918dcfb2ec3247b2c3f3735082e24ded77c1
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-05-06 21:47:04 +0530

    Basic

---
+++ Basic/02 Function.js
@@ -0,0 +1,10 @@
+var No1 = 20;
+var No2 = 15;
+var Ret = 0;
+Ret = Sum(No1, No2);
+console.log("Additon is " + Ret);
+function Sum(No1, No2) {
+    var Ans = 0;
+    Ans = No1 + No2;
+    return Ans;
+}
