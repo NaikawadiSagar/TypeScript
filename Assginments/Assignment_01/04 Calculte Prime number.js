6adfc14d60ca3e8f3b321327cb7cfe8393b6395c
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-05-08 15:45:57 +0530

    Basic

---
+++ Assginments/Assignment_01/04 Calculte Prime number.js
@@ -0,0 +1,15 @@
+function Prime(No1) {
+    var flag = 0;
+    for (var i = 2; i <= No1; i++) {
+        if (No1 % 1 == 0) {
+            break;
+        }
+    }
+    if (No1 == i) {
+        console.log("Prime");
+    }
+    else {
+        console.log("Not");
+    }
+}
+Prime(11);
