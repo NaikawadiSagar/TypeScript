9d2dc1a79ffc5ada8bece591dc27b6f31a66408c
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-05-28 22:55:08 +0530

    Basic Program

---
+++ Function/03 Optional Argument.ts
@@ -0,0 +1,17 @@
+
+//Optional Arguments.
+    
+function Sun(No1:number, No2 ? :number)
+{
+    console.log("Inside Demo");
+    console.log("Value Of No1 : " + No1);
+
+    if(No2!= undefined)
+        {
+            console.log("Value Of No2 : " + No2);
+        }
+    
+}
+
+Sun(10,20);  // No1 = 10   No2 = 20
+Sun(10);     // No1 = 10  No2 = undefined
