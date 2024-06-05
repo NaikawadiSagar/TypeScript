355d9462e67665874b65adb01645c3bcd714013f
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-06-05 20:38:37 +0530

    Practice

---
+++ Types of Function/01 Regular_Function.ts
@@ -0,0 +1,12 @@
+//Regular Function
+
+var Ret:number = 0;
+function Addtion(No1:number, No2:number):number
+{
+    var Ans:number = 0;
+    Ans = No1 + No2;
+    return Ans;
+}
+
+Ret = Addtion(10,11);
+console.log("Addition is :" + Ret);
