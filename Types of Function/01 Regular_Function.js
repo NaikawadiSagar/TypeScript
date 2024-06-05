b243d790d87a183817d37f019b5b5fcb5c4498ff
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-06-05 20:39:13 +0530

    Basic

---
+++ Types of Function/01 Regular_Function.js
@@ -0,0 +1,9 @@
+//Regular Function
+var Ret = 0;
+function Addtion(No1, No2) {
+    var Ans = 0;
+    Ans = No1 + No2;
+    return Ans;
+}
+Ret = Addtion(10, 11);
+console.log("Addition is :" + Ret);
