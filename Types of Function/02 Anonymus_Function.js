e99fbdc0610186b9e98b1367bf8782a956584001
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-06-06 19:57:19 +0530

    Basic

---
+++ Types of Function/02 Anonymus_Function.js
@@ -0,0 +1,9 @@
+//Anonymus FUnction(FUnction Without Name)
+var Ret = 0;
+var Addition2 = function (No1, No2) {
+    var Ans = 0;
+    Ans = No1 + No2;
+    return Ans;
+};
+Ret = Addition2(10, 11);
+console.log("Addition is :" + Ret);
