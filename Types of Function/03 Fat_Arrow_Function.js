d1b26c12a83071baad1f90515e0042eb5b5ce609
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-06-07 16:46:08 +0530

    Practice

---
+++ Types of Function/03 Fat_Arrow_Function.js
@@ -0,0 +1,9 @@
+//Fat Arrow Function OR Arrow OR Labda
+var Ret = 0;
+var Addition3 = function (No1, No2) {
+    var Ans = 0;
+    Ans = No1 + No2;
+    return Ans;
+};
+Ret = Addition3(10, 11);
+console.log("Addition is :" + Ret);
