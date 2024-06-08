30df0bda813ab3ad93e58cdd87990338aa4135bf
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-06-08 12:32:06 +0530

    Basic

---
+++ Array/01 Array.js
@@ -0,0 +1,8 @@
+// inr Arr[5] = {10,20,30,40,50}
+//Arr is ine Dimentionl Arr which contains 5 elements each elements is of type number.
+var Arr = [10, 20, 30, 40, 50];
+console.log("Lenght of Arr is " + Arr.length);
+console.log("Elements in Array is => ");
+for (var i = 0; i < Arr.length; i++) {
+    console.log(Arr[i]);
+}
