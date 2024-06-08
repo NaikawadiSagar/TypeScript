a035a68f67fbb780cccc9f47d9c1ab9138072b08
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-06-08 12:31:20 +0530

    Basic

---
+++ Array/01 Array.ts
@@ -0,0 +1,14 @@
+// inr Arr[5] = {10,20,30,40,50}
+
+//Arr is ine Dimentionl Arr which contains 5 elements each elements is of type number.
+
+var Arr:number[] =[10,20,30,40,50];
+
+console.log("Lenght of Arr is " + Arr.length);
+
+console.log("Elements in Array is => ");
+
+for(var i:number = 0; i < Arr.length; i++)
+{    
+     console.log(Arr[i]);
+}
