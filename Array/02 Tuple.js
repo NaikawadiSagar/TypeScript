9ab193531910c23ab233a736956da5fc3a865638
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-06-09 17:06:22 +0530

    Basic

---
+++ Array/02 Tuple.js
@@ -0,0 +1,6 @@
+var Arr = [10, "Hello", true];
+console.log("lenght of Array is " + Arr.length);
+console.log("Elements in Array is => ");
+for (var i = 0; i < Arr.length; i++) {
+    console.log(Arr[i]);
+}
