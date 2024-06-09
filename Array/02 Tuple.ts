dc733fada616f4928ec49ddc6b56bab16d4e0d8a
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2024-06-09 17:06:07 +0530

    Basic

---
+++ Array/02 Tuple.ts
@@ -0,0 +1,12 @@
+//Tuple of Collection is elements having Any  type of Data Type
+var Arr = [10,"Hello",true];
+
+console.log("lenght of Array is " + Arr.length);
+
+console.log("Elements in Array is => ");
+
+for(var i:number = 0; i < Arr.length; i++)
+{    
+     console.log(Arr[i]);
+}
+
